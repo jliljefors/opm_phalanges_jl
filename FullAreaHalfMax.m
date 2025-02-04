12f45b256257e3614cb4dcf92e42bbf6989062be
Christoph Pfieffer <chrpfe@compute.natmeg.se>
2025-02-04 12:05:17 +0100

    added peak loc to mne FAHM function

--- FullAreaHalfMax.m
+++ FullAreaHalfMax.m
@@ -1,10 +1,12 @@
-function FAHM = FullAreaHalfMax(sourcedistribution,sourcemodel,latency)
+function [FAHM, peak_loc] = FullAreaHalfMax(sourcedistribution,sourcemodel,latency)
 %UNTITLED Calculates the full area at half max amplitude
 %   Detailed explanation goes here
 [~,i_latency] = min(abs(sourcedistribution.time-latency));
 
 % Half max level
-half_max = max(sourcedistribution.avg.pow(:,i_latency))/2;
+[half_max, i_max] = max(sourcedistribution.avg.pow(:,i_latency));
+half_max = half_max/2;
+peak_loc = sourcemodel.pos(i_max,:);
 
 % Find triangles that have at least one point with amplitude >= half max
 i_vertices = find(sourcedistribution.avg.pow(:,i_latency)>=half_max);
